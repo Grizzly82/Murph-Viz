d1df3d12867b756269bdc6714e709fd483156112
Grizzly82 <cris.vizcaino@gmail>
2014-05-28 16:53:57 -0400

    First Obj-C
    Hi this is my first xcode code

---
+++ HelloWorld_ObjC/HelloWorld_ObjCTests/HelloWorld_ObjCTests.m
@@ -0,0 +1,34 @@
+//
+//  HelloWorld_ObjCTests.m
+//  HelloWorld_ObjCTests
+//
+//  Created by Vizcaino, Cristobal {BIS} on 5/28/14.
+//  Copyright (c) 2014 Murph_Viz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloWorld_ObjCTests : XCTestCase
+
+@end
+
+@implementation HelloWorld_ObjCTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
